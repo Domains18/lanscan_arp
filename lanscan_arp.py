a354150515e334f1b1840a677ea9ce9cb5a4d145
Domains18 <gibsonsgibson88@gmail.com>
2023-01-08 20:24:11 +0300

    full script

--- lanscan_arp.py
+++ lanscan_arp.py
@@ -1,2 +1,15 @@
 import scapy.all as scapy
 import re
+print("\n**************************************************************************")
+print("\n* To Whom much Has been Given, much is expected                           *")
+print("\n* https://github.com/Domains18                                            *")
+print("\n***************************************************************************")
+
+ip_add_range_pattern = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
+while True:
+    ip_add_range_entered = input("Enter the IP address range to scan: ")
+    if ip_add_range_pattern.search(ip_add_range_entered):
+        print(f"IP address range entered is valid: {ip_add_range_entered}")
+        break
+    
+arp_result = scapy.arping(ip_add_range_entered)
